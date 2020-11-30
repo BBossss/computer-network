21d674915fee924713d092bcd8dc42bcaaaf5d15
BBossss <1551929492@qq.com>
2020-11-30 00:25:27 -0800

    lab3

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -1,16 +1,10 @@
 #include "tcp_sender.hh"
-
 #include "tcp_config.hh"
-
 #include <random>
 
-// Dummy implementation of a TCP sender
 
 // For Lab 3, please replace with a real implementation that passes the
 // automated checks run by `make check_lab3`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -20,19 +14,106 @@ using namespace std;
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
     : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+    , _stream(capacity)
+    , _retransmission_timeout(retx_timeout){}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return _bytes_in_flight; }
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+    if (!_syn_flag) {
+        TCPSegment seg;
+        seg.header().syn = true;
+        send_segment(seg);
+        _syn_flag = true;
+        return ;
+    }
+
+    size_t win = _window_size > 0 ? _window_size : 1;
+    size_t remain;
+    while ((remain = win - (_next_seqno - _recv_seqno)) != 0 && !_fin_flag) {
+        size_t size = min(TCPConfig::MAX_PAYLOAD_SIZE, remain);
+        TCPSegment seg;
+        string s = _stream.read(size);
+        seg.payload() = Buffer(std::move(s));
+
+        if (seg.length_in_sequence_space() < remain && _stream.eof()) {
+            seg.header().fin = true;
+            _fin_flag = true;
+        }
+
+        if (seg.length_in_sequence_space() == 0)
+            return ;
+
+        send_segment(seg);
+    }
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { 
+    uint64_t abs_ackno = unwrap(ackno, _isn, _recv_seqno);
+    //! check the validity of ackno
+    if (abs_ackno > _next_seqno)
+        return ;
+    _window_size = window_size;
+    if (abs_ackno <= _recv_seqno)
+        return ;
+    _recv_seqno = abs_ackno;
+
+    // ! pop
+    while (!_segments_outstanding.empty()) {
+        TCPSegment seg = _segments_outstanding.front();
+        if (unwrap(seg.header().seqno, _isn, _next_seqno) + seg.length_in_sequence_space() <= abs_ackno) {
+            _bytes_in_flight -= seg.length_in_sequence_space();
+            _segments_outstanding.pop();
+        } else {
+            break;
+        }
+    }
+
+    fill_window();
+
+    _retransmission_timeout = _initial_retransmission_timeout;
+    _consecutive_retransmissions = 0;
+
+    if (!_segments_outstanding.empty()) {
+        _timer_running = true;
+        _timer = 0;
+    }
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    _timer += ms_since_last_tick;
+    if (_timer >= _retransmission_timeout && !_segments_outstanding.empty()) {
+        _segments_out.push(_segments_outstanding.front());
+        if (_window_size != 0)
+            _retransmission_timeout *= 2;
+        _consecutive_retransmissions += 1;
+        _timer_running = true;
+        _timer = 0;
+    }
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+    if (_segments_outstanding.empty())
+        _timer_running = false;
+}
 
-void TCPSender::send_empty_segment() {}
+unsigned int TCPSender::consecutive_retransmissions() const { return _consecutive_retransmissions; }
+
+void TCPSender::send_empty_segment() {
+    TCPSegment seg;
+    seg.header().seqno = wrap(_next_seqno, _isn);
+    _segments_out.push(seg);
+}
+
+void TCPSender::send_segment(TCPSegment &seg) {
+    seg.header().seqno = wrap(_next_seqno, _isn);
+    _next_seqno += seg.length_in_sequence_space();
+    _bytes_in_flight += seg.length_in_sequence_space();
+    _segments_out.push(seg);
+    _segments_outstanding.push(seg);
+    if (!_timer_running) {
+        _timer_running = true;
+        _timer = 0;
+    }
+}
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -32,6 +32,19 @@ class TCPSender {
     //! the (absolute) sequence number for the next byte to be sent
     uint64_t _next_seqno{0};
 
+    //! extra
+    size_t _retransmission_timeout;
+    size_t _bytes_in_flight{0};
+    size_t _consecutive_retransmissions{0};
+    bool _syn_flag{false};
+    bool _fin_flag{false};
+    std::queue<TCPSegment> _segments_outstanding{};
+    bool _timer_running{false};
+    size_t _timer{0};
+    size_t _window_size{1};
+    size_t _recv_seqno{0};
+
+    void send_segment(TCPSegment & seg);
   public:
     //! Initialize a TCPSender
     TCPSender(const size_t capacity = TCPConfig::DEFAULT_CAPACITY,
